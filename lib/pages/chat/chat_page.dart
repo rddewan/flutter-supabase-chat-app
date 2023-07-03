a2a5e173fe4e2ff9c47a715a08f11b010213ef18
Richard Dewan <richarddewan@hotmail.com>
2023-07-03 08:39:44 +0700

    Online and Offline status using channel

--- lib/pages/chat/chat_page.dart
+++ lib/pages/chat/chat_page.dart
@@ -1,7 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_supabase_app/pages/chat/widget/profile_list_widget.dart';
-
+import 'package:flutter_supabase_app/utils/constants.dart';
+import 'package:flutter_supabase_app/utils/snackbar_extension.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({ Key? key }) : super(key: key);
@@ -12,7 +14,60 @@ class ChatPage extends StatefulWidget {
 
 class ChatPageState extends State<ChatPage> {
 
+  final RealtimeChannel _channel = supabase.channel(
+    'chat_app',
+    opts: RealtimeChannelConfig(
+      key: supabase.auth.currentUser?.id ?? ''
+    )
+  );
+
   
+  @override
+  void initState() {    
+    super.initState();
+    _subscribeToChannel();
+  }
+
+  @override
+  void dispose() {
+    _updateOnlineStatus(false, supabase.auth.currentUser?.id ?? '');
+    supabase.removeChannel(_channel);
+    super.dispose();
+  }
+
+  void _subscribeToChannel() {
+    _channel.on(RealtimeListenTypes.presence, ChannelFilter(event: 'sync'), (payload, [ref]) { 
+
+    }).on(RealtimeListenTypes.presence, ChannelFilter(event: 'join'), (payload, [ref]) { 
+      _updateOnlineStatus(true,payload['key']);
+    }).on(RealtimeListenTypes.presence, ChannelFilter(event: 'leave'), (payload, [ref]) { 
+      _updateOnlineStatus(false,payload['key']);
+    }).subscribe((status,[_]) async {
+      
+      if (status == 'SUBSCRIBED') {
+        await _channel.track(
+          {
+            'user': supabase.auth.currentUser?.id,
+            'online_at':  DateTime.now().toIso8601String(),
+          }
+        );
+      }
+    });    
+  }
+
+  void _updateOnlineStatus(bool status, String id) async {
+    try {
+
+      if (id.isEmpty) return;
+
+      await supabase.from('profiles')
+      .update({'is_online': status})
+      .eq('id', id);      
+      
+    } on PostgrestException catch (e) {
+      context.showSnackbar(message: e.message,backgroundColor: Colors.red);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
